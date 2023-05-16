0f79829ecaaaa537863d4999dd6b894c09b6ff0f
Kyle Campbell <kc@kc.io>
2023-05-16 14:49:59 -0700

    Demo Cleanup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+var path = require('path');
+
+const PORT = process.env.PORT || 3000
+const HOST = '0.0.0.0'
+
+// !* Edit here for demos
+const RELEASE_NO = 'PROD-165' 
+
+const api = express()
+
+api.set('views', path.join(__dirname, 'views'));
+api.set('view engine', 'pug');
+
+api.use(express.static(path.join(__dirname, 'public')));
+
+api.get('/', (req, res) => {
+  res.render('index', {
+    release_no: RELEASE_NO
+  })
+})
+
+api.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
